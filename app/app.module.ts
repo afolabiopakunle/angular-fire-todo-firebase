2e92f32f237a49bc34fc68b0385b95d05d30db37
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-11 14:32:36 +0100

    added angular fire

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularFireModule } from '@angular/fire/compat/firebase.app.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    BrowserAnimationsModule, AngularFireModule.initializeApp(environment.firebase)
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
